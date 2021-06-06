852a5e064df86a01adbdc0a8e71819b9676f5ad3
MeerHamza1421 <hamzababar37@gmail.com>
2021-06-06 13:04:08 -0700

    🚀 RELEASE: v1.0.5

--- utils/header.js
+++ utils/header.js
@@ -4,7 +4,7 @@ const pkgJSON = require('../package.json');
 module.exports = () => {
     welcome({
         title: `crypto-currency-cli`,
-        tagLine: `by ${pkgJSON.author}`,
+        tagLine: `by ${pkgJSON.author.name}`,
         description: `${pkgJSON.description}`,
         bgColor: `#00ff00`,
         color: `#000000`,
